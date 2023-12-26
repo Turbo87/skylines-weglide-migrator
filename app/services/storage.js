c4b3b1c1540f9cdb423243490547a556098a9d61
Tobias Bieniek <tobias@bieniek.cloud>
2023-12-26 14:14:50 +0100

    Add flight list

--- app/services/storage.js
+++ app/services/storage.js
@@ -4,6 +4,8 @@ import { tracked } from '@glimmer/tracking';
 const LS_KEY_SKYLINES = 'skylines';
 const LS_KEY_WEGLIDE = 'weglide';
 
+const TIME_THRESHOLD = 1000 * 60 * 5;
+
 export default class StorageService extends Service {
   @tracked skylines = JSON.parse(localStorage.getItem(LS_KEY_SKYLINES));
   @tracked weglide = JSON.parse(localStorage.getItem(LS_KEY_WEGLIDE));
@@ -16,5 +18,33 @@ export default class StorageService extends Service {
   setWeglide(data) {
     this.weglide = data;
     localStorage.setItem(LS_KEY_WEGLIDE, JSON.stringify(data));
+  }
+
+  get flights() {
+    let skylinesFlights = this.skylines?.flights;
+    let weglideFlights = this.weglide?.flights;
+    if (!skylinesFlights || !weglideFlights) {
+      return null;
+    }
+
+    return skylinesFlights.map((skylinesFlight) => {
+      let takeoffTime = Date.parse(skylinesFlight.takeoffTime);
+      let landingTime = Date.parse(skylinesFlight.landingTime);
+
+      let weglideFlight = weglideFlights.find((weglideFlight) => {
+        if (weglideFlight.scoring_date !== skylinesFlight.scoreDate)
+          return false;
+
+        let weglideTakeoffTime = Date.parse(weglideFlight.takeoff_time);
+        let takeoffDelta = Math.abs(weglideTakeoffTime - takeoffTime);
+
+        let weglideLandingTime = Date.parse(weglideFlight.landing_time);
+        let landingDelta = Math.abs(weglideLandingTime - landingTime);
+
+        return takeoffDelta < TIME_THRESHOLD && landingDelta < TIME_THRESHOLD;
+      });
+
+      return { ...skylinesFlight, weglideFlight };
+    });
   }
 }
